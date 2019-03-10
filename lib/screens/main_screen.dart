537ab7fa4ace490e875e77c0f8e357f8659b19fd
Václav Pavlíček <venda.pavlicek@gmail.com>
2019-03-10 20:14:10 +0100

    Add bottom navigation bar to main screen.

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -11,11 +11,13 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Schedule'),
+        title: Text('DevFest CZ 2019'),
       ),
       body: Center(
         child: Container(
@@ -26,6 +28,53 @@ class _MainScreenState extends State<MainScreen> {
             ],
           ),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        fixedColor: Config.colorPalette.shade700,
+        type: BottomNavigationBarType.fixed,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        // new
+        currentIndex: _currentIndex,
+        // new
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            backgroundColor: Colors.amberAccent,
+            icon: Icon(
+              Icons.schedule,
+            ),
+            title: Text(
+              'Schedule',
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.people,
+            ),
+            title: Text(
+              'Speakers',
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.map,
+            ),
+            title: Text(
+              'Maps',
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.business_center,
+            ),
+            title: Text(
+              'Water',
+            ),
+          ),
+        ],
       ),
     );
   }
