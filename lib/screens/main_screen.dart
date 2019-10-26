d90b0eddd14f7289bf0ae3194b2f9598fb848f4d
Vaclav Pavlicek <vaclav@pavlicek.cz>
2019-10-26 21:26:48 +0200

    Remove schedule item from app.

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,13 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:devfest_mobile_app/screens/scan_map_code.dart';
-import 'package:devfest_mobile_app/components/talk_card.dart';
-import 'package:devfest_mobile_app/components/topic.dart';
-import 'package:devfest_mobile_app/components/schedule_item_card.dart';
-import 'package:devfest_mobile_app/components/schedule_time_title.dart';
-import 'package:devfest_mobile_app/entities/talk.dart';
-import 'package:devfest_mobile_app/entities/speaker.dart';
-import 'package:devfest_mobile_app/entities/company.dart';
-import 'package:devfest_mobile_app/entities/schedule_item.dart';
 
 class MainScreen extends StatefulWidget {
   MainScreen({Key key, this.title}) : super(key: key);
@@ -23,120 +15,6 @@ class _MainScreenState extends State<MainScreen> {
 
   Widget decideView() {
     if (_currentIndex == 0) {
-      return ListView(
-        children: <Widget>[
-          ScheduleTimeTitle(
-            hours: "9",
-            minutes: "00",
-          ),
-          ScheduleItemCard(
-            item: ScheduleItem(
-                title: "Keynote",
-                time: "9:00",
-                language: "EN",
-                hall: "Hall A",
-                description:
-                    "Don't miss the opening keynote! Don't miss the opening keynote! Don't miss the opening keynote! Don't miss the opening keynote! Don't miss the opening keynote! Don't miss the opening keynote! Don't miss the opening keynote! Don't miss the opening keynote! Don't miss the opening keynote! Don't miss the opening keynote! Don't miss the opening keynote! Don't miss the opening keynote! Don't miss the opening keynote!"),
-          ),
-          ScheduleTimeTitle(
-            hours: "9",
-            minutes: "30",
-          ),
-          TalkCard(
-            talk: Talk(
-              name: "Why metal is so great? Why metal is so great?",
-              subtitle: "Beginners / EN / Hall A / 40 min",
-              description:
-                  "Accepting payments online should be easy to implement and handy to use. Come and learn how the Google Pay API can improve your payment flow and increase conversions by offering customers tons of helpful check out tools. Join us for an overview session followed by a live demo on how quickly you can integrate Google Pay into your Android application",
-              topics: List<Topic>.from([
-                Topics.android,
-                Topics.firebase,
-              ]),
-              speaker: Speaker(
-                photo: AssetImage("assets/ozzy.jpg"),
-                name: "Ozzy Osbourne",
-                company: Company(
-                  name: "Black Sabbath",
-                  logo: AssetImage("assets/black-sabbath.jpg"),
-                ),
-                position: "Father of Metal",
-                bio:
-                    "Jose is a Developer Programs Engineer on the Google Pay team, currently focusing on facilitating online integration of payment APIs across customers in EMEA. Previously, he worked as a technical trainer and software engineering manager on projects such as Wunderlist, ROI Training and Google Cloud.",
-              ),
-            ),
-          ),
-          TalkCard(
-            talk: Talk(
-              name: "Widgets!",
-              description:
-                  "Accepting payments online should be easy to implement and handy to use. Come and learn how the Google Pay API can improve your payment flow and increase conversions by offering customers tons of helpful check out tools. Join us for an overview session followed by a live demo on how quickly you can integrate Google Pay into your Android application",
-              subtitle: "EN / Hall B",
-              topics: List<Topic>.from([
-                Topics.android,
-                Topics.flutter,
-              ]),
-              speaker: Speaker(
-                name: "Ozzy Osbourne",
-                company: Company(
-                  name: "Black Sabbath",
-                  logo: AssetImage("assets/black-sabbath.jpg"),
-                ),
-                photo: AssetImage("assets/ozzy.jpg"),
-                position: "Father of Metal",
-                bio:
-                    "Jose is a Developer Programs Engineer on the Google Pay team, currently focusing on facilitating online integration of payment APIs across customers in EMEA. Previously, he worked as a technical trainer and software engineering manager on projects such as Wunderlist, ROI Training and Google Cloud.",
-              ),
-            ),
-          ),
-          ScheduleTimeTitle(
-            hours: "10",
-            minutes: "10",
-          ),
-          TalkCard(
-            talk: Talk(
-              name: "Widgets with Firebase!",
-              description:
-                  "Accepting payments online should be easy to implement and handy to use. Come and learn how the Google Pay API can improve your payment flow and increase conversions by offering customers tons of helpful check out tools. Join us for an overview session followed by a live demo on how quickly you can integrate Google Pay into your Android application",
-              subtitle: "Beginners / EN / 40 min",
-              topics: List<Topic>.from([
-                Topics.flutter,
-                Topics.firebase,
-              ]),
-              speaker: Speaker(
-                photo: AssetImage("assets/ozzy.jpg"),
-                name: "Ozzy Osbourne",
-                company: Company(
-                  name: "Black Sabbath",
-                  logo: AssetImage("assets/black-sabbath.jpg"),
-                ),
-                position: "Father of Metal",
-                bio:
-                    "Jose is a Developer Programs Engineer on the Google Pay team, currently focusing on facilitating online integration of payment APIs across customers in EMEA. Previously, he worked as a technical trainer and software engineering manager on projects such as Wunderlist, ROI Training and Google Cloud.",
-              ),
-            ),
-          ),
-          TalkCard(
-            talk: Talk(
-              name: "No topics!",
-              description:
-                  "Accepting payments online should be easy to implement and handy to use. Come and learn how the Google Pay API can improve your payment flow and increase conversions by offering customers tons of helpful check out tools. Join us for an overview session followed by a live demo on how quickly you can integrate Google Pay into your Android application",
-              subtitle: "Beginners / EN / 40 min",
-              speaker: Speaker(
-                name: "Ozzy Osbourne",
-                company: Company(
-                  name: "Black Sabbath",
-                  logo: AssetImage("assets/black-sabbath.jpg"),
-                ),
-                photo: AssetImage("assets/ozzy.jpg"),
-                position: "Father of Metal",
-                bio:
-                    "Jose is a Developer Programs Engineer on the Google Pay team, currently focusing on facilitating online integration of payment APIs across customers in EMEA. Previously, he worked as a technical trainer and software engineering manager on projects such as Wunderlist, ROI Training and Google Cloud.",
-              ),
-            ),
-          ),
-        ],
-      );
-    } else if (_currentIndex == 2) {
       return Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
@@ -332,12 +210,11 @@ class _MainScreenState extends State<MainScreen> {
         // new
         items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            backgroundColor: Colors.amberAccent,
             icon: Icon(
-              Icons.schedule,
+              Icons.business_center,
             ),
             title: Text(
-              'Schedule',
+              'Inventory',
             ),
           ),
           BottomNavigationBarItem(
@@ -346,14 +223,6 @@ class _MainScreenState extends State<MainScreen> {
             ),
             title: Text(
               'Map',
-            ),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.business_center,
-            ),
-            title: Text(
-              'Inventory',
             ),
           ),
           BottomNavigationBarItem(
