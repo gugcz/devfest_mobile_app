70ec5b8c95b1dbde3aefc9c4f678aa32ed9c17c2
Vaclav Pavlicek <vaclav@pavlicek.cz>
2019-10-26 21:47:49 +0200

    Update login screen style.

--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -1,11 +1,5 @@
-import 'dart:async';
-
 import 'package:devfest_mobile_app/config.dart';
-import 'package:devfest_mobile_app/screens/main_screen.dart';
-import 'package:barcode_scan/barcode_scan.dart';
-import 'package:devfest_mobile_app/screens/scan_result.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:devfest_mobile_app/components/devfest_logo.dart';
 import 'package:devfest_mobile_app/components/gug_logo.dart';
 
@@ -30,93 +24,64 @@ class _StartScreenState extends State<StartScreen> {
       backgroundColor: Config.colorPalette.shade500,
       body: Center(
         child: Padding(
-          padding: EdgeInsets.fromLTRB(50, 100, 50, 40),
-          child: Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
+          padding: EdgeInsets.fromLTRB(30, 100, 30, 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Column(children: <Widget>[
                 DevFestLogo(),
-                Container(
+                Padding(
+                  padding: EdgeInsets.only(top: 30),
                   child: Column(
                     children: <Widget>[
                       Text(
-                        'To participate in game you have to scan your QR code that is located at the back of your badge.',
+                        'Enter code located at the back of your badge.',
                         style: TextStyle(
                           color: Colors.white,
                         ),
                         textAlign: TextAlign.center,
                       ),
                       Padding(
-                        padding: EdgeInsets.fromLTRB(0, 30, 0, 30),
-                        child: OutlineButton(
-                          child: Text(
-                            "Scan",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          color: Config.colorPalette.shade50,
-                          splashColor: Config.colorPalette.shade100,
-                          highlightColor: Config.colorPalette.shade100,
-                          onPressed: scan,
-                          borderSide: BorderSide(
-                            color: Colors.white,
-                            width: 1,
-                          ),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: new BorderRadius.circular(7.0),
+                        padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                        child: Container(
+                          width: 100,
+                          child: TextField(
+                            maxLength: 4,
+                            keyboardType: TextInputType.number,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 25),
                           ),
                         ),
                       ),
-                      GestureDetector(
+                      OutlineButton(
                         child: Text(
-                          'Continue without login',
-                          style: TextStyle(
-                            decoration: TextDecoration.underline,
-                            color: Colors.white,
-                          ),
+                          "Enter",
+                          style: TextStyle(color: Colors.white),
                         ),
-                        onTap: () {
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => MainScreen(),
-                            ),
-                          );
-                        },
+                        color: Config.colorPalette.shade50,
+                        splashColor: Config.colorPalette.shade100,
+                        highlightColor: Config.colorPalette.shade100,
+                        onPressed: () {},
+                        borderSide: BorderSide(
+                          color: Colors.white,
+                          width: 1,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(7.0),
+                        ),
                       ),
                     ],
                   ),
                 ),
-                GUGLogo(
-                  opacity: 0.75,
-                ),
-              ],
-            ),
+              ]),
+              GUGLogo(
+                opacity: 0.75,
+              ),
+            ],
           ),
         ),
       ),
+      resizeToAvoidBottomInset: false,
     );
-  }
-
-  Future scan() async {
-    try {
-      String barcode = await BarcodeScanner.scan();
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) => ResultScreen(barcode: barcode),
-        ),
-      );
-    } on PlatformException catch (e) {
-      if (e.code == BarcodeScanner.CameraAccessDenied) {
-        print('The user did not grant the camera permission!');
-      } else {
-        print('Unknown error: $e');
-      }
-    } on FormatException {
-      print(
-          'null (User returned using the "back"-button before scanning anything. Result)');
-    } catch (e) {
-      print('Unknown error: $e');
-    }
   }
 }
