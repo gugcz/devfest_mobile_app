4c15838042eb2295393cf9e0e790e5ff4a22b7ce
Vaclav Pavlicek <vaclav@pavlicek.cz>
2019-10-28 20:41:55 +0100

    Add design of inventory screen.

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,5 +1,5 @@
+import 'package:devfest_mobile_app/config.dart';
 import 'package:flutter/material.dart';
-import 'package:devfest_mobile_app/screens/scan_map_code.dart';
 
 class MainScreen extends StatefulWidget {
   MainScreen({Key key, this.title}) : super(key: key);
@@ -11,229 +11,55 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  int _currentIndex = 0;
-
-  Widget decideView() {
-    if (_currentIndex == 0) {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text("This is inventory screen."),
-        ],
-      );
-    } else {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(6, 3, 6, 3),
-                  child: Card(
-                    child: Padding(
-                      padding: EdgeInsets.all(12),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            '1st',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: 24,
-                            ),
-                          ),
-                          Text(
-                            '123456 - 25 l of water',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: 18,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(6, 3, 6, 3),
-                  child: Card(
-                    child: Padding(
-                      padding: EdgeInsets.all(12),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            '2nd',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: 24,
-                            ),
-                          ),
-                          Text(
-                            '123458 - 20 l of water',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: 18,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(6, 3, 6, 3),
-                  child: Card(
-                    child: Padding(
-                      padding: EdgeInsets.all(12),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            '3rd',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: 24,
-                            ),
-                          ),
-                          Text(
-                            '123457 - 15 l of water',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: 18,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  "You are at position",
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0, 8, 0, 8),
-                  child: Text(
-                    "26",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 42,
-                    ),
-                  ),
-                ),
-                Text(
-                  "with",
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0, 4, 0, 4),
-                  child: Text(
-                    "2",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 36,
-                    ),
-                  ),
-                ),
-                Text(
-                  "l of water.",
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                )
-              ],
-            ),
-          ),
-        ],
-      );
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Config.colorPalette.shade500,
       appBar: AppBar(
         title: Text('DevFest CZ 2019'),
+        backgroundColor: Config.colorPalette.shade700,
       ),
       body: Center(
         child: Container(
-          child: decideView(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Your score:",
+                style: TextStyle(fontSize: 36),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 20,
+                  bottom: 20,
+                ),
+                child: Text(
+                  "26",
+                  style: TextStyle(fontSize: 64),
+                ),
+              ),
+              Text(
+                "litres of water",
+                style: TextStyle(fontSize: 26),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(30, 50, 50, 30),
+                child: Text(
+                  "Scan more QR codes and answer question to receive more points!",
+                  style: TextStyle(fontSize: 18),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        //fixedColor: Config.colorPalette.shade700,
-        type: BottomNavigationBarType.fixed,
-        onTap: (index) {
-          if (index == 1) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => ScanMapCodeScreen(),
-              ),
-            );
-          } else {
-            setState(() {
-              _currentIndex = index;
-            });
-          }
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your onPressed code here!
         },
-        // new
-        currentIndex: _currentIndex,
-        // new
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.business_center,
-            ),
-            title: Text(
-              'Inventory',
-            ),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.map,
-            ),
-            title: Text(
-              'Map',
-            ),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.dashboard,
-            ),
-            title: Text(
-              'Leaderboard',
-            ),
-          ),
-        ],
+        child: Icon(Icons.photo_camera),
+        backgroundColor: Colors.blue,
       ),
     );
   }
