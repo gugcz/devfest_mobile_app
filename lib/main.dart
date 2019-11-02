7656492d5ea2fb38c29a7eab27cf259418743ba6
Vaclav Pavlicek <vaclav@pavlicek.cz>
2019-11-02 21:31:54 +0100

    Make app portrait only.

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:devfest_mobile_app/utils/credentials_file.dart';
 import 'package:devfest_mobile_app/components/decide_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(
@@ -19,6 +20,9 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
     return MaterialApp(
       theme: ThemeData(
         primarySwatch: Config.colorPalette,
